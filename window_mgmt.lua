9319c1ef0ee05525265b2697e9b773deebad1d19
Colin Dean <git@cad.cx>
2015-07-09 21:46:53 -0400

    Adds window centering
    The formula sucks, but I almost never change the grid size.
    
    If someone wants to fix it, please do!

--- window_mgmt.lua
+++ window_mgmt.lua
@@ -13,6 +13,14 @@ mash = {"cmd", "alt", "ctrl"}
 grid.GRIDHEIGHT = 2
 grid.GRIDWIDTH = 4
 
+function centerpoint()
+  return { x = 1, y = 0, w = grid.GRIDWIDTH / 2, h = grid.GRIDHEIGHT }
+end
+
+function interp(s, tab)
+  return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
+end
+
 local grid_shortcuts = {
   [";"] = function() grid.snap(window.focusedwindow()) alert.show("╔═╦═╗\n   s  n \n╠═╬═╣\n   a  p \n╚═╩═╝") end,
   J = function() grid.pushwindow_up() alert.show("🔼") end,
@@ -22,6 +30,11 @@ local grid_shortcuts = {
   I = function() grid.resizewindow_thinner() alert.show("⏩⏪") end,
   O = function() grid.resizewindow_wider() alert.show("⏪⏩") end,
   M = function() grid.maximize_window() alert.show("⏪⏫⏬⏩") end,
+  C = function() grid.set(window.focusedwindow(), centerpoint(), window.focusedwindow():screen()) alert.show("↹") end,
+  G = function()
+        local point = grid.get(window.focusedwindow())
+        alert.show(interp("⬌ ${x} ⬍ ${y}\n\t${w} × ${h}", point))
+      end,
   Y = function() grid.resizewindow_taller() alert.show("⏫⏬") end,
   U = function() grid.resizewindow_shorter() alert.show("⏬⏫") end,
   N = function() grid.pushwindow_nextscreen() alert.show("➡️") end,
